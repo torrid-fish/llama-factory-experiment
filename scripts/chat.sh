e893329b071784e5d6ed2dfe0a942e75f836fd82
Kuan-Ting Wu <torridfish@login1.hpcfund>
2024-09-05 23:35:10 -0500

    DONE

--- scripts/chat.sh
+++ scripts/chat.sh
@@ -1,14 +1,17 @@
+
 # Chat
 if [ "$1" = "lora" ]
 then
-    CUDA_VISIBLE_DEVICES=0,1,2,3 llamafactory-cli webchat \
-        --model_name_or_path Llama3.1-8B-Instruct-hf \
+    CUDA_VISIBLE_DEVICES=0,1,2 llamafactory-cli chat \
+        --model_name_or_path /home/torridfish/Llama3.1-8B-Instruct-hf \
         --template llama3 \
-        --adapter_name_or_path saves/LLaMA3.1-8B-Chat/lora/checkpoint-1600 \
+        --temperature 0 \
+        --adapter_name_or_path checkpoint-1600 \
         --finetuning_type lora
 elif [ "$1" = "base" ]
 then
-    CUDA_VISIBLE_DEVICES=0,1,2,3 llamafactory-cli webchat \
-        --model_name_or_path Llama3.1-8B-Instruct-hf \
+    CUDA_VISIBLE_DEVICES=0,1,2 llamafactory-cli chat \
+        --model_name_or_path /home/torridfish/Llama3.1-8B-Instruct-hf \
+        --temperature 0 \
         --template llama3 
 fi
